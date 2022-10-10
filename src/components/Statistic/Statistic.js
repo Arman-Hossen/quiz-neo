254cd80b070e120a4d7848e02b4abbc53e7216e4
Arman-Hossen <arman15-12726@diu.edu.bd>
2022-10-10 23:33:45 +0600

    Navigation bar

---
+++ src/components/Statistic/Statistic.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Statistic = () => {
+    return (
+        <div>
+            <h1>This Statistics page</h1>
+            
+        </div>
+    );
+};
+
+export default Statistic;
---
+++ src/layouts/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <h2>This is main page</h2>
+
+            
+            
+        </div>
+    );
+};
+
+export default Main;
