a1a70281515cef286212ee33fad4ad65db3441af
Arman-Hossen <arman15-12726@diu.edu.bd>
2022-10-11 17:29:56 +0600

    error fixed

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div className='grid mt-10 ml-5 lg:ml-20  sm:grid-cols-1  lg:grid-cols-3 gap-4  '>
+    <div className='grid md:grid-cols-2 mx-10 sm:grid-cols-1 rounded lg:grid-cols-3 gap-4  '>
             <div className="max-w-xs p-6 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50">
                 <div className="mt-6 mb-2">
                     <span className="block  font-extrabold tracking-widest uppercase dark:text-lime-500 text-xl">What is the purpose of react router</span>
@@ -22,7 +22,7 @@ const Blog = () => {
                 <p className="dark:text-gray-100">Mauris et lorem at elit tristique dignissim et ullamcorper elit. In sed feugiat mi. Etiam ut lacinia dui.
                 Mauris et lorem at elit tristique dignissim et ullamcorper elit. In sed feugiat mi. Etiam ut lacinia dui.Mauris et lorem at elit tristique dignissim et ullamcorper elit. In sed feugiat mi. Etiam ut lacinia dui.</p>
             </div>
-        </div>
+    </div>
   );
 };
 
